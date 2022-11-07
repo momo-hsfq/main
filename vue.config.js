979b44e00628cf7ed8bc0112c514de0d65dee16e
J1aM1ng <2605379072@qq.com>
2022-11-07 11:48:44 +0800

    chore: 更改了前后端交互的一些配置

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 module.exports = {
     publicPath: './',
     productionSourceMap: true,
-    devServer:{
-        port:80,
-        host:'localhost',
-        open:true,
+    devServer: {
+        port: 8080,
+        host: 'localhost',
+        open: true,
+        //设置代理
+        proxy: 'http://localhost:3000',
         // proxy: {
         //     '/api': {
         //         target: 'http://47.93.85.24:8080',
