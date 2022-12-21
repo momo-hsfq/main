eda4cbf602c8b632d180a8715c94e681c8a33274
unknown <2979115538@qq.com>
2022-12-21 13:05:39 +0800

    三级菜单

--- src/components/global/stuMenu.config.js
+++ src/components/global/stuMenu.config.js
@@ -1,60 +1,79 @@
 module.exports = [{
-    name: '课表查询',
-    id: '1',
-    sub: [{
-      name: '学期课表',
-      componentName: 'Stu1_1'
-    }, {
-      name: '教室课表',
-      componentName: 'Stu1_2'
-    }]
+  name: '课表查询',
+  id: '1',
+  sub: [{
+    name: '学期课表',
+    componentName: 'Stu1_1'
   }, {
-    name: '成绩查询',
-    id: '2',
-    sub: [{
-      name: '学期成绩',
-      componentName: 'Stu2'
-    }]
-  }, {
+    name: '教室课表',
+    componentName: 'Stu1_2'
+  }]
+}, {
+  name: '成绩查询',
+  id: '2',
+  sub: [{
+    name: '学期成绩',
+    componentName: 'Stu2'
+  }]
+}, {
+  name: '网上选课',
+  id: '3',
+  sub: [{
     name: '网上选课',
-    id: '3',
-    sub: [{
-      name: '网上选课',
-      componentName: 'Stu3'
-    }]
-  },{
+    componentName: 'Stu3'
+  }]
+},{
+  name: '个人信息',
+  id: '4',
+  sub: [{
     name: '个人信息',
-    id: '4',
-    sub: [{
-      name: '个人信息',
-      componentName: 'Stu4_1'
+    componentName: 'Stu4_1'
+  }, {
+    name: '社会实践',
+    componentName: 'SocialPractice'
+  }, {
+    name: '课外活动',
+    componentName: 'ExtracurricularActivities'
+  }, {
+    name: '成果奖励',
+    componentName: 'AchievementAndAward'
+  }, {
+    name: '修改密码',
+    componentName: 'Stu4_2'
+  }, {
+      name: '换绑手机号',
+      componentName: 'Stu4_3'
     }, {
-      name: '社会实践',
-      componentName: 'SocialPractice'
-    }, {
-      name: '课外活动',
-      componentName: 'ExtracurricularActivities'
-    }, {
-      name: '成果奖励',
-      componentName: 'AchievementAndAward'
-    }, {
-      name: '修改密码',
-      componentName: 'Stu4_2'
-    }, {
-        name: '换绑手机号',
-        componentName: 'Stu4_3'
-      }, {
-        name: '换绑邮箱',
-        componentName:'Stu4_4'
-      }]
-  },{
-    name: '学生互评',
-    id: '5',
-    sub: [{
-      name: '我评价的',
-      componentName: 'PeerAssessment1'
-    }, {
-        name: '评价我的',
-        componentName: 'PeerAssessment2'
+      name: '换绑邮箱',
+      componentName:'Stu4_4'
     }]
+},{
+  name: '学生互评',
+  id: '5',
+  sub: [{
+    name: '我评价的',
+    componentName: 'PeerAssessment1'
+  },{
+      name: '评价我的',
+      componentName: 'PeerAssessment2'
+  }]
+},{
+    name:'学工管理',
+    id: '6',
+    sub: [{
+        name:'学生互评',
+        id:'6-1',
+        sub_sub:[{
+          name:'我评价的',
+          componentName:'PeerAssessment1'
+        },{
+          name:'评价我的',
+          componentName:'PeerAssessment2'
+        }]
+      },{
+        name: '换绑手机号',
+        id:'6-2',
+        componentName: 'Stu4_3'
+      }]
+      
   }]
--- src/components/student/LeftMenuStu.vue
+++ src/components/student/LeftMenuStu.vue
@@ -3,27 +3,50 @@
   <el-menu
       ref="elmenu"
       :default-active="this.$route.path.substring(1)"
+      @open="handleOpen"
+      @close="handleClose"
       unique-opened
       background-color="#547387"
       text-color="#fff"
       active-text-color="#a1c4db"
       router="true">
-      
-      <el-submenu v-for="item in menu" :index="item.id" :key="item.id">
+
+    <template v-for="item in menu">
+      <el-submenu v-if="item.sub" :index="item.id" :key="item.id">
         <template slot="title">
           <span v-text="item.name"></span>
         </template>
-        <el-menu-item-group v-for="sub in item.sub" :key="sub.componentName">
-          <el-menu-item :index="sub.componentName" v-text="sub.name">
-          </el-menu-item>
-        </el-menu-item-group>
+        <template v-for="sub in item.sub">
+          <el-submenu v-if="sub.sub_sub" :index="sub.id" :key="sub.id">
+            <template slot="title">
+              <span>{{sub.name}}</span>
+            </template>
+            <el-menu-item-group v-for="sub_sub in sub.sub_sub" :key="sub_sub.componentName">
+              <template slot="title">
+              <el-menu-item :index="sub_sub.componentName" v-text="sub_sub.name">
+              </el-menu-item>
+              </template>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-menu-item-group v-else :key="sub.componentName">
+            <el-menu-item :index="sub.componentName" v-text="sub.name">
+            </el-menu-item>
+          </el-menu-item-group>
+        </template> 
       </el-submenu>
+      <el-submenu v-else :key="item.componentName">
+        <el-submenu :index="item.componentName" v-text="item.name">
+        </el-submenu>
+      </el-submenu>
+    </template>
+      
 
-    </el-menu>
+  </el-menu>
   </div>
 </template>
 
 <script>
+import { group } from 'console';
 import stuMenu from '../global/stuMenu.config.js'
   export default {
     data(){
